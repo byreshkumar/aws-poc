de8deef85567facba5d27aef9cd418e9c8536913
Byreshkumar Billemane Revanna <BB00469674@techmahindra.com>
2025-02-24 20:44:40 +0530

    test

--- CI/scripts/start_container.sh
+++ CI/scripts/start_container.sh
@@ -1,1 +1,5 @@
+# Pull the Docker image from Docker Hub
+docker pull byreshk/aws-ci-simple-python-flask-app:latest
 
+# Run the Docker image as a container
+docker run -d -p 5000:5000 byreshk/aws-ci-simple-python-flask-app:latest
